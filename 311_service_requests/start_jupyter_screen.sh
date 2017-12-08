c05af3c33f12ff91845f98414e3e3b336c33f7a1
Veera Marni <narayana1043@gmail.com>
2017-12-08 03:33:01 -0500

    adding setup files

---
+++ 311_service_requests/start_jupyter_screen.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "SCREENING NOW"
+echo "Check logs at ~/inovalon/log/jupyter_log.txt"
+screen -dmS jupyter-notebook ~/inovalon/start_jupyter_server.sh:
---
+++ 311_service_requests/start_jupyter_server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export SPARK_HOME=/usr/lib/spark/
+export PYSPARK_PYTHON=/usr/bin/python3.4
+export PYSPARK_DRIVER_PYTHON="jupyter"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+pyspark &> ~/jupyter_log.txt 2>&1
