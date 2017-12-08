3c6de4843db3d4b1fe1a0c24567e0c6c9ec79aa7
Veera Marni <narayana1043@gmail.com>
2017-12-08 04:25:23 -0500

    files updated and removed s3 disctp related

--- 311_service_requests/setup.sh
+++ 311_service_requests/setup.sh
@@ -8,3 +8,7 @@ echo "c.NotebookApp.port = 8889" >> /home/hadoop/.jupyter/jupyter_notebook_confi
 echo "c.NotebookApp.open_browser = False" >> /home/hadoop/.jupyter/jupyter_notebook_config.py
 
 ./setup_files/start_jupyter_screen.sh
+
+aws emr add-steps --cluster-id j-2HFJJ4JQQ5OVG --steps file://./setup_files/s32hdfs.json
+
+
--- 311_service_requests/setup_files/s32hdfs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-aws emr add-steps --cluster-id j-2HFJJ4JQQ5OVG --steps file://./s32hdfs.json
