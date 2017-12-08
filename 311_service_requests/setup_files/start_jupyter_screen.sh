ee087982bacafb3ef69631e49289c1c906913aa8
Veera Marni <narayana1043@gmail.com>
2017-12-08 09:19:05 +0000

    typo

--- 311_service_requests/setup_files/start_jupyter_screen.sh
+++ 311_service_requests/setup_files/start_jupyter_screen.sh
@@ -2,4 +2,4 @@
 
 echo "SCREENING NOW"
 echo "Check logs at ~/jupyter_log.txt"
-screen -dmS jupyter-notebook ./setup_files/start_jupyter_server.sh:
+screen -dmS jupyter-notebook ./setup_files/start_jupyter_server.sh
