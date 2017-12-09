af1d5a0e129e548cc8c4860e8b04e064dbfc10b7
Veera Marni <narayana1043@gmail.com>
2017-12-09 06:34:52 +0000

    system hanged

--- 311_service_requests/setup_files/start_jupyter_server.sh
+++ 311_service_requests/setup_files/start_jupyter_server.sh
@@ -4,4 +4,4 @@ export SPARK_HOME=/usr/lib/spark/
 export PYSPARK_PYTHON=/usr/bin/python3.4
 export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
-pyspark &> ~/jupyter_log.txt 2>&1
+pyspark --master yarn &> ~/jupyter_log.txt 2>&1
