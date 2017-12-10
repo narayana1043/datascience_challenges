464656d28ebb54658e5fa0f8e1a17747b70e1a21
Veera Marni <narayana1043@gmail.com>
2017-12-10 04:31:22 -0500

    minor update

--- 311_service_requests/setup_files/install_and_configure_git.sh
+++ 311_service_requests/setup_files/install_and_configure_git.sh
@@ -7,6 +7,5 @@ git config --global user.name "Veera Marni"
 git config --global user.email narayana1043@gmail.com
 git clone https://github.com/narayana1043/datascience_challenges.git
 cd datascience_challenges
-git remote remove origin
 
 # follow instructions in get_data.sh if data is not is s3
