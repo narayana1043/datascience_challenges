f96056a0b59ca8e6bbbac52a9f063d18f7051604
Veera Marni <narayana1043@gmail.com>
2017-12-10 04:35:16 -0500

    added pandas

--- 311_service_requests/setup.sh
+++ 311_service_requests/setup.sh
@@ -4,6 +4,7 @@
 
 sudo yum install screen
 sudo pip-3.4 install jupyter
+sudo pip-3.4 install pandas
 jupyter notebook --generate-config
 echo "c.NotebookApp.port = 8889" >> /home/hadoop/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> /home/hadoop/.jupyter/jupyter_notebook_config.py
