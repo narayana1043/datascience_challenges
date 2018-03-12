894bbcbd5a7f4fea8001e55a115c316576072fa5
Veera Marni <narayana1043@gmail.com>
2018-03-11 20:44:11 -0400

    added dist metrics

--- groupon_practice/kMeans.py
+++ groupon_practice/kMeans.py
@@ -4,19 +4,31 @@ import pandas as pd
 
 # utils
 
-def eclud_dist(X, center, distance='ecluid'):
-    if distance.lower() == 'ecluid':
-        return np.sqrt(np.sum(np.square(X - center), axis=1))
-    else:
-        return None
+def eclud_dist(A, b):
+    return np.sqrt(np.sum(np.square(A - b), axis=1))
+
+
+def manhat_dist(A, b):
+    return np.sum(np.abs(A - b), axis=1)
+
+
+def max_dist(A, b):
+    return np.max(np.abs(A - b), axis=1)
+
 
 
 class KMeans:
 
-    def __init__(self, n_means=3, max_iter=100):
+    def __init__(self, n_means=3, distance_metric='ecludiean', max_iter=1000):
         self.n_means = n_means
         self.max_iter = max_iter
         self.centers = None
+        if distance_metric.lower() == 'manhattan':
+            self.distance = manhat_dist
+        elif distance_metric.lower() == 'maximum':
+            self.distance = max_dist
+        else:
+            self.distance = eclud_dist
 
     def fit(self, X):
         center_indices = np.random.choice(X.shape[0], self.n_means, replace=False)
@@ -26,7 +38,7 @@ class KMeans:
         for iteration in range(self.max_iter):
             dist = []
             for center in self.centers:
-                dist.append(eclud_dist(X, center))
+                dist.append(self.distance(X, center))
             category = np.argmin(dist, axis=0)
 
             for center_id in range(self.n_means):
@@ -36,7 +48,7 @@ class KMeans:
     def predict(self, Y):
         dist = []
         for center in self.centers:
-            dist.append(eclud_dist(Y, center))
+            dist.append(self.distance(Y, center))
         return np.argmin(dist, axis=0)
 
 
@@ -45,27 +57,31 @@ data = pd.read_csv('./data/iris.data.csv', names=[0,1,2,3,4])
 
 # print(data.head())
 
-clf = KMeans()
-clf.fit(data[[0,1,2,3]])
-print(clf.predict(data[[0,1,2,3]]))
+# clf = KMeans()
+# clf.fit(data[[0,1,2,3]])
+# print(clf.predict(data[[0,1,2,3]]))
 
 
 # sample test
 
-# sample_x = pd.DataFrame([[1, 2],
-#               [1.5, 1.8],
-#               [5, 8],
-#               [8, 8],
-#               [1, 1.06],
-#               [9, 11],
-#               [1, 3],
-#               [8, 9],
-#               [0, 3],
-#               [5, 4],
-#               [6, 4], ], columns=[0,1])
-# clf.fit(sample_x)
-# print(clf.centers)
-# print(clf.predict(sample_x))
+sample_x = pd.DataFrame([[1, 2],
+              [1.5, 1.8],
+              [5, 8],
+              [8, 8],
+              [1, 1.06],
+              [9, 11],
+              [1, 3],
+              [8, 9],
+              [0, 3],
+              [5, 4],
+              [6, 4], ], columns=[0,1])
+
+for dist in ['manhattan', 'maximum', 'ecludiean']:
+    clf = KMeans(distance_metric=dist)
+    print('\nMetric:\t',dist)
+    clf.fit(sample_x)
+    print(clf.centers)
+    print(clf.predict(sample_x))
 # sample_x.plot.scatter(0,1)
 # for center in clf.centers:
 #     plt.scatter(center[0], center[1], color='r')
